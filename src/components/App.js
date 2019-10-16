2017eddb309fcea31e61cb55b69ae1b21ab6a7fc
erib <eri-b@users.noreply.github.com>
2019-10-16 09:31:53 -0400

    reorg css files, deleted icons, default props for bg color

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Display from './Display'
 import ButtonPanel from './ButtonPanel'
 import calculate from '../logic/calculate.js'
-import './app.css'
+import '../css/app.css'
 
 export default class App extends React.Component {
   render(){

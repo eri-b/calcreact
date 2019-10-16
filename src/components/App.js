89fa9fe62cc00535fb0038914d2c22b5f40e5259
erib <eri-b@users.noreply.github.com>
2019-10-16 09:40:53 -0400

    commented out unused import temporarily

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Display from './Display'
 import ButtonPanel from './ButtonPanel'
-import calculate from '../logic/calculate.js'
+// import calculate from '../logic/calculate.js'
 import '../css/app.css'
 
 export default class App extends React.Component {
